ce692a1b54531e91b205f2cd08abdafe81d11e03
pranav <pranav@pranav-PC>
2016-11-07 23:12:50 -0500

    Used Future with Callable. this enables Tasks to have return value which will be set in future variable.

--- src/com/practice/concurrancy/WorkQueue.java
+++ src/com/practice/concurrancy/WorkQueue.java
@@ -1,16 +1,18 @@
 package com.practice.concurrancy;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+
 public class WorkQueue {
 
-	private BlockingQueue<Runnable> queue=null;
+	private BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
 	private ThreadPoolExecutor executor = null;
 	public WorkQueue(int corePoolSize,int maximumPoolSize,int keepAliveTime){
-		queue = new LinkedBlockingQueue<Runnable>();
 		executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS, queue);
 	}
 
@@ -21,4 +23,14 @@ public class WorkQueue {
 			System.err.println("Exception While Executng the thread..."+e);
 		}
 	}
+	
+	public Future<?> submit(Callable<?> r){
+		try {
+			return executor.submit(r);
+		} catch (Exception e) {
+			System.err.println("Exception While Executng the thread..."+e);
+		}
+		return null;
+	}
+	
 }
--- src/com/practice/concurrancy/WorkQueueImple.java
+++ src/com/practice/concurrancy/WorkQueueImple.java
@@ -1,43 +1,56 @@
 package com.practice.concurrancy;
 
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 public class WorkQueueImple {
 	int i = 0;
 	WorkQueue wq = new WorkQueue(20, 40, 15*60);
-	public static void main(String[] args) {
-		(new WorkQueueImple()).startWork(); 
+	static ArrayList<Future<String>> f = new ArrayList<>();
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		WorkQueueImple impl = new WorkQueueImple();
+		impl.startWork2();
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for(Future<String> ft:f){
+			System.out.println(ft.get());
+		}
 	}
 	
-	public void startWork(){
+	public void startWork1(){
 		System.out.println("Starting....");
 		for (int i =0; i<100;i++){
 			DoWork work = new DoWork(i);
-			System.err.println("Start Process:--"+i);
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			wq.execute(work);
-			System.err.println("End Process:--"+i);
 		}
 		
 	}
 	
-	
-	
-	public class DoWork implements Runnable {
-		int workerId;
-		public DoWork(int id) {
-			this.workerId = id;
-		}
-		
-		@Override
-		public void run() {
-			System.out.println("Processing work.... "+workerId);
+	public void startWork2(){
+		System.out.println("Starting....");
+		for (int i =0; i<100;i++){
+			DoWorkWithResult work = new DoWorkWithResult(i);
 			try {
-				Thread.sleep(5000);
+				Thread.sleep(5);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			System.out.println("Done Processing work.... "+workerId);
+			f.add((Future<String>) wq.submit(work));
 			
 		}
 		
 	}
+	
+	
+	
 }
